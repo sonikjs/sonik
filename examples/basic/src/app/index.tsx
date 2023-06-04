0155d1642b74b8faf1fc0037120de8d3d5369d4d
Yusuke Wada <yusuke@cloudflare.com>
2023-06-04 22:07:04 +0900

    make it simpler

--- examples/basic/src/app/index.tsx
+++ examples/basic/src/app/index.tsx
@@ -1,8 +1,5 @@
 import type { Route } from 'nashi'
 
 export default {
-  GET: (c) => {
-    const name = c.req.query('name')
-    return <h1>Hello {name}!</h1>
-  },
+  GET: () => <h1>Hello!</h1>,
 } satisfies Route
