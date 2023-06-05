8159376e98123eb8a4b55c6f9d495f01a36ec215
Yusuke Wada <yusuke@cloudflare.com>
2023-06-05 19:20:48 +0900

    update tests

--- packages/nashi/src/utils.ts
+++ packages/nashi/src/utils.ts
@@ -1,6 +1,8 @@
 export const filePathToPath = (filePath: string) => {
   return filePath
-    .replace(/\/src\/app|index|\.tsx$/g, '')
+    .replace(/\/src\/app|\.tsx$/g, '')
+    .replace(/^\/index/, '/') // `/index`
+    .replace(/\/index/, '') // `/about/index`
     .replace(/\[\.{3}.+\]/, '*')
     .replace(/\[(.+)\]/, ':$1')
 }
--- packages/nashi/test/utils.test.ts
+++ packages/nashi/test/utils.test.ts
@@ -1,5 +1,20 @@
-describe('foo', () => {
-  it('Should return true', () => {
-    expect(true).toBe(true)
+import { filePathToPath } from '../src/utils'
+
+describe('filePathToPath', () => {
+  it('Should return a correct path', () => {
+    expect(filePathToPath('/src/app/index.tsx')).toBe('/')
+    expect(filePathToPath('/src/app/about.tsx')).toBe('/about')
+    expect(filePathToPath('/src/app/about/index.tsx')).toBe('/about')
+    expect(filePathToPath('/src/app/about/me')).toBe('/about/me')
+    expect(filePathToPath('/src/app/about/me/index.tsx')).toBe('/about/me')
+    expect(filePathToPath('/src/app/about/me/address.tsx')).toBe('/about/me/address')
+  })
+  it('Should return a correct path - with slug', () => {
+    expect(filePathToPath('/src/app/[slug].tsx')).toBe('/:slug')
+    expect(filePathToPath('/src/app/posts/[slug].tsx')).toBe('/posts/:slug')
+  })
+  it('Should return a correct path - with catch-all', () => {
+    expect(filePathToPath('/src/app/[...slug].tsx')).toBe('/*')
+    expect(filePathToPath('/src/app/posts/[...slug].tsx')).toBe('/posts/*')
   })
 })
