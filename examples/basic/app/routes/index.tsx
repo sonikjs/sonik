c6f20694e5896f8d1f8d688a951a38203fd7dd71
Yusuke Wada <yusuke@cloudflare.com>
2023-06-16 01:11:42 +0900

    simplify

--- examples/basic/app/routes/index.tsx
+++ examples/basic/app/routes/index.tsx
@@ -1,10 +1,9 @@
 import Counter from '../islands/Counter'
-import type { Context } from 'sonik'
 
-export default function Home(c: Context) {
+export default function Home() {
   return (
     <div>
-      <h2>Hello {c.req.query('name')}!</h2>
+      <h2>Hello!</h2>
       <Counter />
     </div>
   )
