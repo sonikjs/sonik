18c51ea9130c263a4f1ef9dc3b551b494b5d6859
Yusuke Wada <yusuke@cloudflare.com>
2023-08-06 18:23:33 +0900

    chore(example): update

--- examples/basic/app/routes/_layout.tsx
+++ examples/basic/app/routes/_layout.tsx
@@ -6,7 +6,7 @@ const handler: LayoutHandler = (_, { children, head }) => {
       <head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link href='/static/global.css' rel='stylesheet' />
-        {head.createHeadTag()}
+        {head.createTags()}
       </head>
       <body>
         <div class='wrapper'>
