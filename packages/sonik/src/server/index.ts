d875d88c9069b3c2a2feb4061809760c5e828423
Yusuke Wada <yusuke@cloudflare.com>
2023-08-06 09:28:42 +0900

    refactor: don't use class

--- packages/sonik/src/server/index.ts
+++ packages/sonik/src/server/index.ts
@@ -1,17 +1,1 @@
-import type { Hono } from '../types'
-import type { ServerOptions } from './server'
-import { Server } from './server'
-
-type CreateAppOptions = Partial<{
-  app: Hono
-}> &
-  ServerOptions
-
-export function createApp(options?: CreateAppOptions) {
-  const server = options
-    ? new Server({
-        ...options,
-      })
-    : new Server()
-  return server.createApp({ app: options?.app })
-}
+export { createApp } from './server'
--- packages/sonik/src/server/server.ts
+++ packages/sonik/src/server/server.ts
@@ -21,83 +21,64 @@ export type ServerOptions = Partial<{
   ROUTES: Record<string, RouteFile>
   root: string
   renderToString: RenderToString
+  app: Hono
 }>
 
 type RenderToString = (node: Node) => string
-
-type Dir = string
-type FileName = string
-
 type RouteFile = { default: FC & Route }
-type RouteMap = Record<Dir, Record<FileName, RouteFile>>
-
 type LayoutFile = { default: LayoutHandler }
-type LayoutList = Record<Dir, FileName[]>
-
 type PreservedFile = { default: ErrorHandler | Handler }
-type PreservedMap = Record<Dir, Record<FileName, PreservedFile>>
+
+type ToWebOptions = {
+  head: Head
+  layouts?: string[]
+  filename: string
+}
 
 const addDocType = (html: string) => {
   return `<!doctype html>${html}`
 }
 
-export class Server {
-  private PRESERVED: Record<string, PreservedFile>
-  private LAYOUTS: Record<string, LayoutFile>
-  private preservedMap: PreservedMap
-  private layoutList: LayoutList
-  private routesMap: RouteMap
-  private root: string
-  private render: RenderToString
+export const createApp = <E extends Env>(options?: ServerOptions): Hono<E> => {
+  const PRESERVED =
+    options?.PRESERVED ??
+    import.meta.glob('/app/routes/**/(_error|_404).(tsx)', {
+      eager: true,
+    })
 
-  constructor(options?: ServerOptions) {
-    // `import.meta.glob` can only use literals
-    this.PRESERVED =
-      options?.PRESERVED ??
-      import.meta.glob('/app/routes/**/(_error|_404).(tsx)', {
-        eager: true,
-      })
+  const preservedMap = groupByDirectory(PRESERVED)
 
-    this.preservedMap = groupByDirectory(this.PRESERVED)
+  const LAYOUTS =
+    options?.LAYOUTS ??
+    import.meta.glob('/app/routes/**/_layout.tsx', {
+      eager: true,
+    })
 
-    this.LAYOUTS =
-      options?.LAYOUTS ??
-      import.meta.glob('/app/routes/**/_layout.tsx', {
-        eager: true,
-      })
+  const layoutList = listByDirectory(LAYOUTS)
 
-    this.layoutList = listByDirectory(this.LAYOUTS)
+  const ROUTES =
+    options?.ROUTES ??
+    import.meta.glob('/app/routes/**/[a-z0-9[-][a-z0-9[_-]*.(tsx|mdx)', {
+      eager: true,
+    })
 
-    const ROUTES =
-      options?.ROUTES ??
-      import.meta.glob('/app/routes/**/[a-z0-9[-][a-z0-9[_-]*.(tsx|mdx)', {
-        eager: true,
-      })
+  const routesMap = groupByDirectory(ROUTES)
 
-    this.routesMap = groupByDirectory(ROUTES)
+  const root = options?.root ?? '/app/routes'
 
-    this.root = options?.root ?? '/app/routes'
+  let render: RenderToString
 
-    if (options?.renderToString) {
-      this.render = options?.renderToString
-    } else {
-      this.render = (node: Node) => node.toString()
-    }
+  if (options?.renderToString) {
+    render = options?.renderToString
+  } else {
+    render = (node: Node) => node.toString()
   }
 
-  private toWebResponse = async (
+  const toWebResponse = async (
     c: Context,
     res: Node | Promise<Node> | Response | Promise<Response>,
     status: number = 200,
-    {
-      layouts,
-      head,
-      filename,
-    }: {
-      head: Head
-      layouts?: string[]
-      filename: string
-    }
+    { layouts, head, filename }: ToWebOptions
   ) => {
     if (res instanceof Promise) res = await res
     if (res instanceof Response) return res
@@ -107,135 +88,131 @@ export class Server {
         return b.split('/').length - a.split('/').length
       })
       for (const path of layouts) {
-        const layout = this.LAYOUTS[path]
+        const layout = LAYOUTS[path]
         if (layout) {
           res = await layout.default(c, { children: res, head, filename })
         }
       }
-      const html = this.render(res)
+      const html = render(res)
       return c.html(addDocType(html), status)
     }
 
-    const defaultLayout = this.LAYOUTS[this.root + '/_layout.tsx']
+    const defaultLayout = LAYOUTS[root + '/_layout.tsx']
     if (defaultLayout) {
-      const html = this.render(await defaultLayout.default(c, { children: res, head, filename }))
+      const html = render(await defaultLayout.default(c, { children: res, head, filename }))
       return c.html(addDocType(html), status)
     }
 
-    const html = this.render(res)
+    const html = render(res)
     return c.html(html, status)
   }
 
-  createApp = <E extends Env>(options?: { app?: Hono }): Hono<E> => {
-    const app = options?.app ?? new Hono()
+  const app = options?.app ?? new Hono()
 
-    for (const [dir, content] of Object.entries(this.routesMap)) {
-      const subApp = new Hono()
+  for (const [dir, content] of Object.entries(routesMap)) {
+    const subApp = new Hono()
 
-      let layouts = this.layoutList[dir]
+    let layouts = layoutList[dir]
 
-      const getLayoutPaths = (paths: string[]) => {
-        layouts = this.layoutList[paths.join('/')]
-        if (!layouts) {
-          paths.pop()
-          if (paths.length) {
-            getLayoutPaths(paths)
-          }
-        }
-      }
-
+    const getLayoutPaths = (paths: string[]) => {
+      layouts = layoutList[paths.join('/')]
       if (!layouts) {
-        const dirPaths = dir.split('/')
-        getLayoutPaths(dirPaths)
-      }
-
-      for (const [filename, route] of Object.entries(content)) {
-        const routeDefault = route.default
-        if (!routeDefault) continue
-
-        const path = filePathToPath(filename)
-        const head = new Head()
-
-        const options = {
-          layouts,
-          head,
-          filename,
+        paths.pop()
+        if (paths.length) {
+          getLayoutPaths(paths)
         }
-
-        // Function Component
-        if (typeof routeDefault === 'function') {
-          subApp.get(path, (c) => {
-            const res = routeDefault(c, { head })
-            return this.toWebResponse(c, res, 200, options)
-          })
-        }
-
-        for (const [method, handler] of Object.entries(routeDefault)) {
-          if (method === 'APP') {
-            const appHandler = routeDefault['APP']
-            if (appHandler) {
-              appHandler(subApp.use(path))
-            }
-          } else {
-            if (handler) {
-              subApp.on(method, path, async (c, next) => {
-                const res = await (handler as Handler)(c, { head, next })
-                return this.toWebResponse(c, res, 200, options)
-              })
-            }
-          }
-        }
-
-        for (const [preservedDir, content] of Object.entries(this.preservedMap)) {
-          if (dir !== this.root && dir === preservedDir) {
-            const notFound = content[NOTFOUND_FILENAME]
-            if (notFound) {
-              const notFoundHandler = notFound.default as NotFoundHandler
-              subApp.get('*', (c) =>
-                this.toWebResponse(c, notFoundHandler(c, { head }), 404, options)
-              )
-            }
-            const error = content[ERROR_FILENAME]
-            if (error) {
-              const errorHandler = error.default as ErrorHandler
-              subApp.onError((error, c) =>
-                this.toWebResponse(c, errorHandler(c, { error, head }), 500, options)
-              )
-            }
-          }
-        }
-
-        const regExp = new RegExp(`^${this.root}`)
-        app.route(dir.replace(regExp, ''), subApp)
       }
     }
 
-    const head = new Head()
-
-    if (this.preservedMap[this.root]) {
-      const defaultNotFound = this.preservedMap[this.root][NOTFOUND_FILENAME]
-      if (defaultNotFound) {
-        const notFoundHandler = defaultNotFound.default as NotFoundHandler
-        app.notFound((c) =>
-          this.toWebResponse(c, notFoundHandler(c, { head }), 404, {
-            head,
-            filename: NOTFOUND_FILENAME,
-          })
-        )
-      }
-
-      const defaultError = this.preservedMap[this.root][ERROR_FILENAME]
-      if (defaultError) {
-        const errorHandler = defaultError.default as ErrorHandler
-        app.onError((error, c) =>
-          this.toWebResponse(c, errorHandler(c, { error, head }), 500, {
-            head,
-            filename: ERROR_FILENAME,
-          })
-        )
-      }
+    if (!layouts) {
+      const dirPaths = dir.split('/')
+      getLayoutPaths(dirPaths)
     }
 
-    return app as unknown as Hono<E>
+    for (const [filename, route] of Object.entries(content)) {
+      const routeDefault = route.default
+      if (!routeDefault) continue
+
+      const path = filePathToPath(filename)
+      const head = new Head()
+
+      const options = {
+        layouts,
+        head,
+        filename,
+      }
+
+      // Function Component
+      if (typeof routeDefault === 'function') {
+        subApp.get(path, (c) => {
+          const res = routeDefault(c, { head })
+          return toWebResponse(c, res, 200, options)
+        })
+      }
+
+      for (const [method, handler] of Object.entries(routeDefault)) {
+        if (method === 'APP') {
+          const appHandler = routeDefault['APP']
+          if (appHandler) {
+            appHandler(subApp.use(path))
+          }
+        } else {
+          if (handler) {
+            subApp.on(method, path, async (c, next) => {
+              const res = await (handler as Handler)(c, { head, next })
+              return toWebResponse(c, res, 200, options)
+            })
+          }
+        }
+      }
+
+      for (const [preservedDir, content] of Object.entries(preservedMap)) {
+        if (dir !== root && dir === preservedDir) {
+          const notFound = content[NOTFOUND_FILENAME]
+          if (notFound) {
+            const notFoundHandler = notFound.default as NotFoundHandler
+            subApp.get('*', (c) => toWebResponse(c, notFoundHandler(c, { head }), 404, options))
+          }
+          const error = content[ERROR_FILENAME]
+          if (error) {
+            const errorHandler = error.default as ErrorHandler
+            subApp.onError((error, c) =>
+              toWebResponse(c, errorHandler(c, { error, head }), 500, options)
+            )
+          }
+        }
+      }
+
+      const regExp = new RegExp(`^${root}`)
+      app.route(dir.replace(regExp, ''), subApp)
+    }
   }
+
+  const head = new Head()
+
+  if (preservedMap[root]) {
+    const defaultNotFound = preservedMap[root][NOTFOUND_FILENAME]
+    if (defaultNotFound) {
+      const notFoundHandler = defaultNotFound.default as NotFoundHandler
+      app.notFound((c) =>
+        toWebResponse(c, notFoundHandler(c, { head }), 404, {
+          head,
+          filename: NOTFOUND_FILENAME,
+        })
+      )
+    }
+
+    const defaultError = preservedMap[root][ERROR_FILENAME]
+    if (defaultError) {
+      const errorHandler = defaultError.default as ErrorHandler
+      app.onError((error, c) =>
+        toWebResponse(c, errorHandler(c, { error, head }), 500, {
+          head,
+          filename: ERROR_FILENAME,
+        })
+      )
+    }
+  }
+
+  return app as unknown as Hono<E>
 }
