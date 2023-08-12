1b75e48cdc5ab6e57c474a2b2af9c25473d4b50b
Yusuke Wada <yusuke@cloudflare.com>
2023-08-12 23:15:00 +0900

    refactor: simplify preact preset

--- packages/sonik/src/presets/preact/index.ts
+++ packages/sonik/src/presets/preact/index.ts
@@ -1,11 +1,8 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import type { Env } from 'hono'
-import { Fragment, createElement, h, options as preactOptions } from 'preact'
+import { Fragment, createElement } from 'preact'
 import type { VNode } from 'preact'
 import { render } from 'preact-render-to-string'
-import { COMPONENT_NAME, DATA_SERIALIZED_PROPS } from '../../constants.js'
 import { createApp as baseCreateApp } from '../../server/index.js'
-import { serialize } from '../../server/serializer.js'
 import type { ServerOptions } from '../../server/server.js'
 import type * as types from '../../types.js'
 
@@ -28,36 +25,3 @@ export type ErrorHandler<E extends Env = Env> = types.ErrorHandler<E, Node>
 export type LayoutHandler = types.LayoutHandler<Node>
 export type FC<E extends Env = Env> = types.FC<E, Node>
 export type Route<E extends Env = Env> = types.Route<E, Node>
-
-/** Serialize server values into nodes */
-const DONE = '__done'
-const oldHook = preactOptions.vnode
-let data = {}
-preactOptions.vnode = (vnode) => {
-  // @ts-ignore
-  if (typeof vnode.type === 'function' && !vnode.props[DONE]) {
-    const originalType = vnode.type
-    vnode.type = (props) => {
-      data = serialize(props)
-      return h(originalType, props)
-    }
-    // @ts-ignore
-    vnode.props[DONE] = true
-  }
-
-  // @ts-ignore
-  if (vnode.props[COMPONENT_NAME] && !vnode.props[DONE]) {
-    const originalType = vnode.type
-    vnode.type = (props) => {
-      const serializedData: Record<string, string> = {}
-      // @ts-ignore
-      delete data[DONE]
-      serializedData[DATA_SERIALIZED_PROPS] = JSON.stringify(data)
-      // @ts-ignore
-      return h(originalType, { ...props, ...serializedData })
-    }
-    // @ts-ignore
-    vnode.props[DONE] = true
-  }
-  if (oldHook) oldHook(vnode)
-}
