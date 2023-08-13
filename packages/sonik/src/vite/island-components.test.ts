4575a8324801ea120b269ab84c3193333dba5de0
Yusuke Wada <yusuke@cloudflare.com>
2023-08-13 09:28:33 +0900

    test: fix island-component test

--- packages/sonik/src/vite/island-components.test.ts
+++ packages/sonik/src/vite/island-components.test.ts
@@ -11,9 +11,8 @@ describe('transformJsxTags', () => {
       `const BadgeOriginal = function () {
   return <h1>Hello</h1>;
 };
-const WrappedBadge = props => {
-  const serializedProps = JSON.stringify(props);
-  return <div component-wrapper="true"><div component-name="Badge.tsx" data-serialized-props={serializedProps}><BadgeOriginal {...props}></BadgeOriginal></div></div>;
+const WrappedBadge = function (props) {
+  return import.meta.env.SSR ? <div component-name="Badge.tsx" data-serialized-props={JSON.stringify(props)}><BadgeOriginal {...props}></BadgeOriginal></div> : <BadgeOriginal {...props}></BadgeOriginal>;
 };
 export default WrappedBadge;`
     )
