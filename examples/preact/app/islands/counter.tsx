71dc07964ffd6e2de61232892d1c229201270afa
Yusuke Wada <yusuke@cloudflare.com>
2023-08-13 09:33:52 +0900

    chore(example): bump up sonik

--- examples/preact/app/islands/counter.tsx
+++ examples/preact/app/islands/counter.tsx
@@ -5,9 +5,9 @@ export default function Counter() {
   const increment = () => setCount(count + 1)
 
   return (
-    <div>
+    <>
       <p>Count: {count}</p>
       <button onClick={increment}>Increment</button>
-    </div>
+    </>
   )
 }
