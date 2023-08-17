fc3068df70a471f999b699e1fbc8857e683ee956
Yusuke Wada <yusuke@cloudflare.com>
2023-08-17 23:19:33 +0900

    fix(react): fix sonikify

--- packages/sonik/src/presets/react/sonikify.tsx
+++ packages/sonik/src/presets/react/sonikify.tsx
@@ -6,18 +6,20 @@ export const sonikify = <P extends {}>(
   comp: (p: P) => JSX.Element,
   componentName?: string
 ): ((props: P & { children?: JSX.Element }) => JSX.Element) => {
-  return (arg: P) => {
-    if (import.meta.env.SSR) {
-      const inner = { __html: renderToString(createElement(comp as any, arg)) }
-      const Div = (
-        <div
-          component-name={componentName}
-          data-serialized-props={JSON.stringify(arg)}
-          dangerouslySetInnerHTML={inner}
-        ></div>
-      )
-      return Div as any
+  if (import.meta.env.SSR) {
+    return (arg: P) => {
+      if (import.meta.env.SSR) {
+        const inner = { __html: renderToString(createElement(comp as any, arg)) }
+        const Div = (
+          <div
+            component-name={componentName}
+            data-serialized-props={JSON.stringify(arg)}
+            dangerouslySetInnerHTML={inner}
+          ></div>
+        )
+        return Div as any
+      }
     }
-    return comp
   }
+  return comp
 }
