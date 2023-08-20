305e4187300834777a439f9a580b3482594c3a26
Yusuke Wada <yusuke@cloudflare.com>
2023-08-20 19:45:32 +0900

    chore(example): update api example

--- examples/api/app/routes/about/index.ts
+++ examples/api/app/routes/about/index.ts
@@ -2,8 +2,8 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get((c) => {
-  const name = c.req.param<'/:name'>('name')
+app.get('/:name', (c) => {
+  const name = c.req.param('name')
   return c.json({
     'your name is': name,
   })
