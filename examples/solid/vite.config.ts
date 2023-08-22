ed2471b28b6ee8a05119ecfba42cc2b7908fda8f
Yusuke Wada <yusuke@cloudflare.com>
2023-08-22 11:39:40 +0900

    chore(example): update the solid example

--- examples/solid/vite.config.ts
+++ examples/solid/vite.config.ts
@@ -1,18 +1,14 @@
 import sonik from 'sonik/vite'
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
+import pages from 'sonik/cloudflare-pages'
 
 export default defineConfig({
-  ssr: {
-    external: ['solid-js', 'solid-js/web'],
-  },
   plugins: [
     solidPlugin({
       ssr: true,
     }),
-    sonik({
-      entry: './_worker.ts',
-      minify: true,
-    }),
+    sonik(),
+    pages(),
   ],
 })
