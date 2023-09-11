830cb49485591000bd4eb826e6dd52c8366ac350
Yusuke Wada <yusuke@cloudflare.com>
2023-09-11 22:36:57 +0900

    test: fixed test

--- packages/sonik/test/hono-jsx/app/routes/about/[name].tsx
+++ packages/sonik/test/hono-jsx/app/routes/about/[name].tsx
@@ -8,11 +8,15 @@ export const route = defineRoute((app) => {
   })
 })
 
-export default function Books(c: Context) {
+export default function AboutName(c: Context) {
+  const { name } = c.req.param()
   return c.render(
-    <form method='POST'>
-      <input type='text' name='title' />
-      <input type='submit' />
-    </form>
+    <>
+      <p>It's {name}</p>
+      <Badge name={name} />
+    </>,
+    {
+      title: name,
+    }
   )
 }
