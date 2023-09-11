9dd2d30ad9c524735a3efe795046a0455c15ff9b
Yusuke Wada <yusuke@cloudflare.com>
2023-09-11 22:54:32 +0900

    chore(example-basic): simplify

--- examples/basic/app/routes/index.tsx
+++ examples/basic/app/routes/index.tsx
@@ -1,20 +1,4 @@
-import { Context, defineRoute } from 'sonik'
-
-export const route = defineRoute((app) => {
-  app.get((c) => {
-    return c.render(
-      <div>
-        <h2>
-          Hello <a href='/about/me'>me</a>!
-        </h2>
-      </div>,
-      {
-        title: 'Welcome to Sonik!',
-        meta: [{ name: 'description', content: 'This an example for Sonik' }],
-      }
-    )
-  })
-})
+import { Context } from 'sonik'
 
 export default function Index(c: Context) {
   return c.render(
