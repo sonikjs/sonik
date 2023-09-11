d74c7213c28cdd2fe8ba3883a9685025cc508212
Yusuke Wada <yusuke@cloudflare.com>
2023-09-11 23:21:16 +0900

    test(react): update tests

--- packages/preset-react/test/app/routes/about/[name].tsx
+++ packages/preset-react/test/app/routes/about/[name].tsx
@@ -1,7 +1,7 @@
 import { defineRoute } from '../../../../src'
 import Badge from '../../components/Badge'
 
-export default defineRoute((app) => {
+export const route = defineRoute((app) => {
   app.get((c) => {
     const { name } = c.req.param<'/about/:name'>()
     return c.render(
