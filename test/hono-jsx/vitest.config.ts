9c89c51c723dae63bdae6e239df12ef39c99f2f4
Yusuke Wada <yusuke@kamawada.com>
2023-12-15 15:47:08 +0900

    don't test mdx

--- test/hono-jsx/vitest.config.ts
+++ test/hono-jsx/vitest.config.ts
@@ -1,14 +1,6 @@
-import mdx from '@mdx-js/rollup'
 import { defineConfig } from 'vite'
 import { islandComponents } from '../../src/vite/island-components'
 
 export default defineConfig({
-  plugins: [
-    islandComponents(),
-    {
-      ...mdx({
-        jsxImportSource: 'hono/jsx',
-      }),
-    },
-  ],
+  plugins: [islandComponents()],
 })
